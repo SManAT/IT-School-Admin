844d7ebf6e7f78e18c8b5052d4ce975e79dd57e1
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2021-05-31 10:20:23 +0200

    work

--- MySQL/backup_mysql.py
+++ MySQL/backup_mysql.py
@@ -1,7 +1,8 @@
-import subprocess
+import yaml
 import os
 
 print("Hallo")
+"""
 --single-transaction uses a consistent read and guarantees that data seen by mysqldump does not change.
 
 Backup in single Files
@@ -10,3 +11,18 @@ for DB in $(mysql - u root - p mysqlmaster - e 'show databases' - s - -skip-colu
 do
 mysqldump $DB > "$DB.sql"
 done
+"""
+
+
+def load_yml():
+    """
+    Load the yaml file config.yaml
+    """
+    with open('config.yaml', 'rt') as f:
+        yml = yaml.safe_load(f.read())
+    return yml
+
+
+if __name__ == "__main__":
+    config = load_yml()
+    print(config)
