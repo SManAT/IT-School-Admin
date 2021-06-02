f51bf1ee866f4823cd16bf5a35cb8c73c8107aa4
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2021-06-02 20:50:54 +0200

    working

--- MySQL/MySQLBackup.py
+++ MySQL/MySQLBackup.py
@@ -2,10 +2,13 @@ import yaml
 import os
 from pathlib import Path
 from libs.CmdRunner import CmdRunner
-import inspect
+from datetime import date
+import sys
 
 
 class MySQLBackup():
+    
+    prefix = "mysql-backup-"
 
     def __init__(self):
         self.rootDir = Path(__file__).parent
@@ -14,7 +17,7 @@ class MySQLBackup():
         info = ("MySQLBackup\n"
         "(c) Mag. Stefan Hagmann 2021\n"
         "this tool is creating mysqldumps of all databases\n"
-        "=================================================\n\n") 
+        "=================================================\n") 
         print(info)
 
         try:
@@ -42,6 +45,18 @@ class MySQLBackup():
             path = os.path.join(self.rootDir, path)
         if os.path.isdir(path) is False:
             os.makedirs(path)
+            
+        # create dump-YYYY-MM-DD directory
+        today = date.today()
+        path = os.path.join(path, "%s%s" % (self.prefix, today.strftime("%Y-%m-%d")))
+        if os.path.isdir(path) is False:
+            os.makedirs(path)
+        else:
+            print("Das Backup %s gibt es bereits" % path)
+            print("-exit-")
+            sys.exit()
+        
+        self.backup_path = path
 
     def backupDB(self):
         """ Backup all Databases in a Directory with Logrotate """
@@ -52,8 +67,20 @@ class MySQLBackup():
         if errors:
             print(errors)
         databases = runner.getLines()
+        
+        unwanted_db = {"sys", "information_schema"}
+        databases = [ele for ele in databases if ele not in unwanted_db]
         for db in databases:
-            print(db)
+            if db.strip() is not "":
+                print("Backup DB: %s" % db)
+                cmd = "mysqldump --defaults-extra-file=mysqldump.cnf --single-transaction %s > %s/%s.sql" % (db, self.backup_path, db)
+                # print("%s\n" % cmd)
+                os.system(cmd)
+                
+        print("-done-")
+            
+            
+            
 
 
 if __name__ == "__main__":
@@ -63,10 +90,6 @@ if __name__ == "__main__":
 """
 --single-transaction uses a consistent read and guarantees that data seen by mysqldump does not change.
 
-Backup in single Files
-
-for DB in $(mysql - u root - p mysqlmaster - e 'show databases' - s - -skip-column-names)
-do
-mysqldump $DB > "$DB.sql"
-done
+IMPORT
+mysql -u root -p -e'flush privileges;'
 """
