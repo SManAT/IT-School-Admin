b3153ce2abc5a68a000744a92be6447a5e61ae86
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2021-08-05 13:37:06 +0200

    Update setHostname.py

--- Cloning-Stuff/setHostname/setHostname.py
+++ Cloning-Stuff/setHostname/setHostname.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import os
-from pathlib import Path 
+from pathlib import Path
 import argparse
 
 import yaml
