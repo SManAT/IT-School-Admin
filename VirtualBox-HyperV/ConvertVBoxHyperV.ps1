023f177b959c422ef74fa163cde9df74b68d8d47
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2022-01-16 16:35:16 +0100

    Update ConvertVBoxHyperV.ps1

--- VirtualBox-HyperV/ConvertVBoxHyperV.ps1
+++ VirtualBox-HyperV/ConvertVBoxHyperV.ps1
@@ -1,9 +1,8 @@
-$VBoxPath = "C:\Program Files\Oracle\VirtualBox\"
-$HddPath = "H:\WinServer20220\Clients\DC1\"
+$HddPath = "F:\Virtual Machines\VirtualBox\Machines\life-2021-10-Klon\"
 $TargetPath = "H:\HyperV\Virtual Hard Disks\"
+$Hdd = "life-2021-10-Klon-disk1.vdi"
 
-$Hdd = "WinServer20220.vdi"
-
+$VBoxPath = "C:\Program Files\Oracle\VirtualBox\"
 # dont change after this line -----------------------
 
 
@@ -14,26 +13,29 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 Write  'Running PS with Administrator Rights'
 #Run as Admin--------------------
 
-
 $TargetHdd = $Hdd -replace ".vdi$", ".vhd"
 
+$TPath = Join-Path -Path $TargetPath -ChildPath $TargetHdd
+$HPath = Join-Path -Path $HddPath -ChildPath $Hdd
+
 # Delete Medium if exists
-$AllArgs = @('closemedium', 'disk', '$TargetPath$TargetHdd', '--delete')
+$AllArgs = @('closemedium', 'disk', '$TPath', '--delete')
 Invoke-Expression "& '$($VBoxPath)VBoxManage.exe' $AllArgs"
      
 # ----------------------------
-Write-Output "Convert $HddPath$Hdd > $TargetPath$TargetHdd"
+Write-Output "Convert $HPath > $TPath"
 
-$AllArgs = @('clonemedium', '$HddPath$Hdd', '$TargetPath$TargetHdd', '--format VHD')
+$AllArgs = @('clonemedium', '$HPath', '$TPath', '--format VHD')
 Invoke-Expression "& '$($VBoxPath)VBoxManage.exe' $AllArgs"
 
 # ----------------------------
 # to VHDX wit Cmdlet
 $TargetHddX = $TargetHdd -replace ".vhd$", ".vhdx"
-Convert-VHD -Path $TargetPath$TargetHdd -DestinationPath $TargetPath$TargetHddX
+$TXPath = Join-Path -Path $TargetPath -ChildPath $TargetHddX
+Convert-VHD -Path $TPath -DestinationPath $TXPath
 
 # Delete File vhd
-Remove-Item $TargetPath$TargetHdd
+Remove-Item $TPath
 
 Write-Output "Exit in 5 Sekunden ..."
 Start-Sleep -s 5
