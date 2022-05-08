4c31d524149baede6f13d34df2b09ea3e3044743
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2022-05-08 08:50:04 +0200

    work

--- Windows-Server/Active-Domain/Usermanagement/ps/addUser.ps1
+++ Windows-Server/Active-Domain/Usermanagement/ps/addUser.ps1
@@ -1,14 +1,29 @@
 Import-Module ActiveDirectory
-New-ADUser `
-  -Name "%NACHNAME% %VORNAME%" `
-  -GivenName "%VORNAME%" `
-  -Surname "%NACHNAME%" `
-  -UserPrincipalName "%USERNAME%" `
-  -AccountPassword (ConvertTo-SecureString "%PASSWORD%" -AsPlainText -Force) `
-  -Path "OU=%GRUPPE_SHORT%,%OUUSERS%" `
-  -ChangePasswordAtLogon 1 `
-  -Enabled 1 `
-  -DisplayName "%VORNAME% %NACHNAME%" `
-  -HomeDrive "%HOMEDRIVE%" `
-  -HomeDirectory "%HOMEDIR%%username%" `
-  -ProfilePath "%PROFILEDIR%%username%"
+# User accounts, by default, are created without a password. 
+# If you provide a password, an attempt will be made to set that password however, this can fail due to password policy 
+# restrictions. The user account will still be created and you may use Set-ADAccountPassword to set the password on 
+# that account. In order to ensure that accounts remain secure, user accounts will never be enabled unless a 
+# valid password is set or PasswordNotRequired is set to $True.
+# The account is created if the password fails for any reason.
+
+$PW_ = "%PASSWORD%"
+$ADSplat = @{
+    UserPrincipalName     = "%USERNAME%"
+    Name                  = "%NACHNAME% %VORNAME%"
+    GivenName             = "%VORNAME%"
+    Surname               = "%NACHNAME%"
+    Enabled               = $True
+    DisplayName           = "%VORNAME% %NACHNAME%"
+    Country               = "AT"
+    Path                  = "OU=%GRUPPE_SHORT%,%OUUSERS%"
+    ChangePasswordAtLogon = 1
+    AccountPassword       = (ConvertTo-SecureString $PW_ -AsPlainText -Force)
+    HomeDrive             = "%HOMEDRIVE%"
+    HomeDirectory         = "%HOMEDIR%%username%"
+    ProfilePath           = "%PROFILEDIR%%username%"
+}
+
+New-ADUser @ADSplat
+
+#$NewPwd = ConvertTo-SecureString "%PASSWORD%" -AsPlainText -Force
+#Set-ADAccountPassword -Identity "%NACHNAME% %VORNAME%" -NewPassword $NewPwd -Reset
