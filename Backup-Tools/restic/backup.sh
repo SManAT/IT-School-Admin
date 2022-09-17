dd4342bcee94287c6e1cb7c14e1f44cedffecd48
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2022-09-17 18:53:15 +0200

    Update backup.sh

--- Backup-Tools/restic/backup.sh
+++ Backup-Tools/restic/backup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-
 REPOSITORY=10.0.10.5:/Backups/Webhost
 PWDFILE=/root/.ssh/restic.pwd
 
 restic -r sftp:sshUser@$REPOSITORY backup --files-from include.txt --exclude-file exclude.txt -p $PWDFILE -v
+
+# keep n snapshots
+restic -r sftp:sshUser@$REPOSITORY forget --keep-last  4 -p $PWDFILE
+
+# free space
+restic -r sftp:sshUser@$REPOSITORY prune --keep-last  4 -p $PWDFILE
