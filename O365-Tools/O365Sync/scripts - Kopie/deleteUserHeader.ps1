f5e369c9da2b9bb72506aaea7c549009ed7a3413
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2022-11-02 10:37:35 +0100

    work

--- O365-Tools/O365Sync/scripts - Kopie/deleteUserHeader.ps1
+++
@@ -1,4 +0,0 @@
-$User = "{% username %}"
-$strPass = ConvertTo-SecureString -String "{% password %}" -AsPlainText -Force
-$UserCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($User, $strPass)
-Connect-AzureAD -Credential $UserCredential
